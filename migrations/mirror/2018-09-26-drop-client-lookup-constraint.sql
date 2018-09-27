c920ff6136cc0d073049c42672f00f6f43740577
Nonzaliseko Phamane <nonzaliseko.phamane@imqs.co.za>
2018-09-27 16:04:09 +0200

    Remove size constraint on mirror lookups for wip

---
+++ migrations/mirror/2018-09-26-drop-client-lookup-constraint.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "refComponent_Region" ALTER COLUMN "k" TYPE VARCHAR;
+ALTER TABLE "refComponent_Facility" ALTER COLUMN "k" TYPE VARCHAR;
+ALTER TABLE "refComponent_Department" ALTER COLUMN "k" TYPE VARCHAR;
+ALTER TABLE "refComponent_Supplier" ALTER COLUMN "k" TYPE VARCHAR;
+ALTER TABLE "refComponent_Ward" ALTER COLUMN "k" TYPE VARCHAR;
+ALTER TABLE "refComponent_Suburb" ALTER COLUMN "k" TYPE VARCHAR;
+ALTER TABLE "refProject_CostCentre" ALTER COLUMN "k" TYPE VARCHAR;
