06587dee8425a4e0e82557ec42e097997f800545
FritzOnFire <fritz.engl@gmail.com>
2019-05-22 17:33:56 +0200

    Moved surface type for road db

---
+++ migrations/road/2019-05-22-moved-surface-type.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "road" ADD COLUMN "surf_type" VARCHAR;
+CREATE INDEX ON "road" ("rcam_class");
+CREATE INDEX ON "road" ("surf_type");
+CREATE INDEX ON "road" ("pavement_type");
+ALTER TABLE "road_static" DROP COLUMN "surf_type";
