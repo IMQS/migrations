de39d122c43a9d7fa2b9a6ee67396a43ea7415fb
Nkosilathi Vundla <nkosimarz@gmail.com>
2019-05-23 17:35:40 +0200

    Remove wrong fully created table, data properly mapped to rainfall

---
+++ migrations/main/2019-05-23-remove-raingage-table.sql
@@ -0,0 +1,10 @@
+DROP TABLE "StormWaterRainGage_01";
+DROP TABLE "StormWaterRainGage_02";
+DROP TABLE "StormWaterRainGage_03";
+DROP TABLE "StormWaterRainGage_04";
+DROP TABLE "StormWaterRainGage_05";
+DROP TABLE "StormWaterRainGage_06";
+DROP TABLE "StormWaterRainGage_07";
+DROP TABLE "StormWaterRainGage_08";
+DROP TABLE "StormWaterRainGage_09";
+DROP TABLE "StormWaterRainGage_10";
