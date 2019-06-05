2194a79a2b24592bb8acaf904d9770d33a9382ff
FritzOnFire <fritz.engl@gmail.com>
2019-06-05 11:13:17 +0200

    TI-3514: Fix for failing migration. Migration failed because of a view using the colum being droped.

--- migrations/road/2019-05-22-moved-surface-type.sql
+++ migrations/road/2019-05-22-moved-surface-type.sql
@@ -2,4 +2,4 @@ ALTER TABLE "road" ADD COLUMN "surf_type" VARCHAR;
 CREATE INDEX ON "road" ("rcam_class");
 CREATE INDEX ON "road" ("surf_type");
 CREATE INDEX ON "road" ("pavement_type");
-ALTER TABLE "road_static" DROP COLUMN "surf_type";
+ALTER TABLE "road_static" DROP COLUMN "surf_type" CASCADE;
