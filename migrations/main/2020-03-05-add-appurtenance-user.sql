37c69860ea401da7fff37df3e3f21636ef018f25
nkosimarz <nkosi.vundla@imqs.co.za>
2020-03-05 18:30:12 +0200

    Add appurtenances user table and show sewer from and to id

---
+++ migrations/main/2020-03-05-add-appurtenance-user.sql
@@ -0,0 +1,90 @@
+CREATE TABLE "WaterAuxAppurtenanceUser_01" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_01" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_02" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_02" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_03" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_03" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_04" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_04" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_05" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_05" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_06" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_06" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_07" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_07" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_08" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_08" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_09" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_09" ("ID");
+
+CREATE TABLE "WaterAuxAppurtenanceUser_10" (
+ "rowid" BIGSERIAL,
+ "Locality" VARCHAR,
+ "MetaID" INTEGER,
+ "ID" UUID,
+ PRIMARY KEY("rowid")
+);
+CREATE UNIQUE INDEX ON "WaterAuxAppurtenanceUser_10" ("ID");
+
