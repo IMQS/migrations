8d15a4563775a6d453bc44a6e57eff883ab25df5
FritzOnFire <fritz.engl@gmail.com>
2020-07-07 11:17:46 +0200

    Reports schema changes
    Removes deprecation fin values

---
+++ migrations/reports/2020-07-07-remove-depreciation-fin-values.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "assets"."EnvelopeFin" DROP COLUMN "DepreciationOpening";
+ALTER TABLE "assets"."EnvelopeFin" DROP COLUMN "DepreciationFinYTD";
+ALTER TABLE "assets"."EnvelopeFin" DROP COLUMN "DepreciationClosing";
+ALTER TABLE "assets"."LandparcelFin" DROP COLUMN "DepreciationOpening";
+ALTER TABLE "assets"."LandparcelFin" DROP COLUMN "DepreciationFinYTD";
+ALTER TABLE "assets"."LandparcelFin" DROP COLUMN "DepreciationClosing";
+ALTER TABLE "assets"."LandparcelFin" DROP COLUMN "DepreciationMethodID";
