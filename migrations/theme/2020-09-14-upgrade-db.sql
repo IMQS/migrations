d868c6f12a0c74e6dbe0fe445965eb98ebc5a344
FritzOnFire <fritz.engl@gmail.com>
2020-09-18 13:50:14 +0200

    New migration works

--- migrations/theme/2020-09-14-upgrade-db.sql
+++ migrations/theme/2020-09-14-upgrade-db.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
 CREATE TABLE "ElementHierarchy" (
  "id" UUID,
  "parent" UUID,
@@ -8,10 +10,10 @@ CREATE TABLE "ElementHierarchy" (
 );
 
 ALTER TABLE "Data" DROP CONSTRAINT "Data_pkey";
-DROP INDEX "Data_rowid_idx";
-DROP INDEX "Data_identity_idx";
+DROP INDEX "idx_Data_identity";
 DROP INDEX "idx_Data_identity_C";
 ALTER TABLE "Data" ADD COLUMN "id" UUID;
+UPDATE "Data" SET "id" = gen_random_uuid();
 ALTER TABLE "Data" ADD PRIMARY KEY ("id");
 ALTER TABLE "Data" ADD COLUMN "hierarchy" UUID;
 ALTER TABLE "Data" ADD COLUMN "metadata" JSONB;
@@ -21,19 +23,17 @@ CREATE UNIQUE INDEX ON "Data" ("id");
 ALTER TABLE "Data" RENAME COLUMN "identity" TO "name";
 
 ALTER TABLE "Theme" DROP CONSTRAINT "Theme_pkey";
-DROP INDEX "Theme_rowid_idx";
-DROP INDEX "Theme_identity_idx";
+DROP INDEX "idx_Theme_identity";
 DROP INDEX "idx_Theme_identity_C";
 ALTER TABLE "Theme" ADD COLUMN "id" UUID;
+UPDATE "Theme" SET "id" = gen_random_uuid();
 ALTER TABLE "Theme" ADD PRIMARY KEY ("id");
-ALTER TABLE "Theme" ADD COLUMN "name" VARCHAR;
 ALTER TABLE "Theme" ADD COLUMN "owner" VARCHAR;
 ALTER TABLE "Theme" ADD COLUMN "modules" VARCHAR;
 ALTER TABLE "Theme" ADD COLUMN "hash" BYTEA;
 ALTER TABLE "Theme" ADD COLUMN "hierarchy" UUID;
 ALTER TABLE "Theme" ADD COLUMN "metadata" JSONB;
 ALTER TABLE "Theme" DROP COLUMN "rowid";
-ALTER TABLE "Theme" DROP COLUMN "identity";
 CREATE UNIQUE INDEX ON "Theme" ("id");
 
 ALTER TABLE "Theme" RENAME COLUMN "identity" TO "name";
