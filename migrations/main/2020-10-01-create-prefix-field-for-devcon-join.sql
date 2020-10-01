06847071b49937a02a5076de4f344681289ea247
Jason K. George <jasonkofo@gmail.com>
2020-10-01 11:10:20 +0200

    INF-1046: Migrate WaterDemandStand for potential Devcon functionality
    Decided that it wouldn't hurt making the same set of chagne

--- migrations/main/2020-10-01-create-prefix-field-for-devcon-join.sql
+++ migrations/main/2020-10-01-create-prefix-field-for-devcon-join.sql
@@ -1,3 +1,7 @@
 ALTER TABLE "LandCadastral" ADD COLUMN IF NOT EXISTS "SG_ID_PREFIX" VARCHAR;
 UPDATE "LandCadastral" SET "SG_ID_PREFIX" = SUBSTRING("SG_ID", 1, 11);
 CREATE INDEX ON "LandCadastral" ("SG_ID_PREFIX");
+
+ALTER TABLE "WaterDemandStand" ADD COLUMN IF NOT EXISTS "Stand_ID_Prefix" VARCHAR;
+UPDATE "WaterDemandStand" SET "Stand_ID_Prefix" = SUBSTRING("Stand_ID", 1, 11);
+CREATE INDEX ON "WaterDemandStand" ("Stand_ID_Prefix");
