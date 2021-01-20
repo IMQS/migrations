72c606e5325687fb39006028acb08c4f764a9d5c
helaine <helaine.du.plessis@EOH.com>
2021-01-20 22:06:14 +0200

    Fixed a typo

--- migrations/main/2021-01-14-alterations-for-spatial-linkrer.sql
+++ migrations/main/2021-01-14-alterations-for-spatial-linkrer.sql
@@ -6,6 +6,6 @@ ALTER TABLE "LinkingMainAndMirror" ADD COLUMN "left_id" VARCHAR;
 UPDATE "LinkingMainAndMirror" SET "left_id" = "main_id";
 
 
-ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_id" VARCHAR;
+ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_id";
 ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_table_name";
 ALTER TABLE "LinkingMainAndMirrorTrackTable" ADD COLUMN "left_dbname" VARCHAR;
