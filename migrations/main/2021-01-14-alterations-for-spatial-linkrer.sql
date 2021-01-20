0af51714337ec053cfeb54f12afe3619ec48a8ed
helaine <helaine.du.plessis@EOH.com>
2021-01-20 17:26:04 +0200

    Copy values from main_id to left_id delete main_id

--- migrations/main/2021-01-14-alterations-for-spatial-linkrer.sql
+++ migrations/main/2021-01-14-alterations-for-spatial-linkrer.sql
@@ -1,7 +1,11 @@
 ALTER TABLE "LinkingMainAndMirror" ADD COLUMN "left_table_name" VARCHAR;
 ALTER TABLE "LinkingMainAndMirror" ADD COLUMN "left_dbname" VARCHAR;
 ALTER TABLE "LinkingMainAndMirror" ADD COLUMN "left_id_column" VARCHAR;
-ALTER TABLE "LinkingMainAndMirror" RENAME "main_id" TO "left_id";
-ALTER TABLE "LinkingMainAndMirror" ALTER COLUMN "left_id" TYPE VARCHAR;
+ALTER TABLE "LinkingMainAndMirror" ADD COLUMN "left_id" VARCHAR;
+
+UPDATE "LinkingMainAndMirror" SET "left_id" = "main_id";
+
+
+ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_id" VARCHAR;
 ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_table_name";
 ALTER TABLE "LinkingMainAndMirrorTrackTable" ADD COLUMN "left_dbname" VARCHAR;
