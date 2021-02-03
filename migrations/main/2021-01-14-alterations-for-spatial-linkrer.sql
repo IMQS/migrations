4beda2f60e881131d11c5ac3efd4263b6074e76e
Helaine <heliduplessis@gmail.com>
2021-02-03 10:31:16 +0200

    Replace view dependant on main_id

--- migrations/main/2021-01-14-alterations-for-spatial-linkrer.sql
+++ migrations/main/2021-01-14-alterations-for-spatial-linkrer.sql
@@ -9,8 +9,17 @@ UPDATE "LinkingMainAndMirror" SET "left_table_name" = "main_table_name";
 DROP VIEW IF EXISTS "sewer-gravity";
 DROP VIEW IF EXISTS "sewer-rising";
 DROP VIEW IF EXISTS "water-pipe";
+DROP VIEW IF EXISTS "sewer-pipe";
 
-DROP RULE  IF EXISTS "_RETURN ON reports.mismatch_assets_to_asbuilt" ON "LinkingMainAndMirror"
+CREATE OR REPLACE VIEW reports.mismatch_assets_to_asbuilt
+ AS
+ SELECT "LinkingMainAndMirror".left_table_name,
+        "LinkingMainAndMirror".left_id,
+        "LinkingMainAndMirror".assetmapfeature_id,
+        "LinkingMainAndMirror".assetcomponent_id
+FROM    "LinkingMainAndMirror"
+WHERE   "LinkingMainAndMirror".left_id IS NOT NULL AND "LinkingMainAndMirror".assetcomponent_id IS NOT NULL;
+
 ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_id";
 ALTER TABLE "LinkingMainAndMirror" DROP COLUMN "main_table_name";
 ALTER TABLE "LinkingMainAndMirrorTrackTable" ADD COLUMN "left_dbname" VARCHAR;
